c7e74b3829d29fc625dfa066b4d9698213f7dcac
cstasney <cstasney07@gmail.com>
2023-11-05 18:57:29 -0500

    class constructor for image shape and text

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-// Import file system, graceful-fs, and shape modules
+// Import file system, fs, and shape modules
 
 const fs = require('fs')
 const inquirer = require("inquirer");
 const {Circle, Square, Triangle} = require("./lib/shapes");
 
+// SVG class constructor w/ 3 methods to render and set text in SVG string
+
+class SVG {
+    constructor() {
+        this.elementShape = ''
+        this.elementText = ''
+    }
+    render(){
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width ="300" height="200">${this.elementShape}${this.elementText}</svg>`
+    }
+    setElementText(text, color) {
+        this.elementText = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    } 
+    setElementShape(shape) {
+        this.elementShape = shape.render()
+    }
+}
