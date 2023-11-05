c7b73f82116c1709eefa861f65f50dcf285ca3a7
cstasney <cstasney07@gmail.com>
2023-11-05 18:49:57 -0500

    file structuting and imports

---
+++ index.js
@@ -0,0 +1,6 @@
+// Import file system, graceful-fs, and shape modules
+
+const fs = require('fs')
+const inquirer = require("inquirer");
+const {Circle, Square, Triangle} = require("./lib/shapes");
+
