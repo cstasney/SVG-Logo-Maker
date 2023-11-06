117b45f22d9c5cc012c7bacdb492c1e64c0cbe8f
cstasney <cstasney07@gmail.com>
2023-11-05 22:29:22 -0500

    cleanup

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const { Circle, Square, Triangle } = require("./lib/shapes");
 
 class SVG {
     constructor() {
-        this.elementShape = ''
         this.elementText = ''
+        this.elementShape = ''
     }
     render() {
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width ="300" height="200">${this.elementShape}${this.elementText}</svg>`
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.elementShape}${this.elementText}</svg>`
     }
     setElementText(text, color) {
         this.elementText = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
@@ -85,13 +85,13 @@ async function init() {
 
     // desired shape
     let userShape;
-    if (shape === "Square" || shape === "square" || shape === "SQAURE") {
+    if (shape === "Square") {
         userShape = new Square();
         console.log("User selected Square")
-    } else if (shape === "Circle" || shape === "circle" || shape === "CIRCLE") {
+    } else if (shape === "Circle") {
         userShape = new Circle();
         console.log("User selected Circle");
-    } else if (shape === "Triangle" || shape === "triangle" || shape === "TRIANGLE") {
+    } else if (shape === "Triangle") {
         userShape = new Triangle();
         console.log("User selected Triangle");
     }
@@ -101,11 +101,11 @@ async function init() {
     var svg = new SVG();
     svg.setElementText(userInput, fontColor);
     svg.setElementShape(userShape);
-    svgString=svg.render();
+    svgString = svg.render();
 
     // console log string once file has been written
     console.log("Displaying Shape:\n\n" + svgString);
-    
+
     // console log successful generation
     console.log("Logo Generation Successful!")
     generateLogo(svg_file, svgString)
